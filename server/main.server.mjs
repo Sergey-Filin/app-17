045746ac2c84382fa7d4b6ce1c0a146ef2df0cee
Serhii <sergeyfilin9999@gmail.com>
2024-01-17 00:01:10 +0200

    Auto-generated commit [ci skip]

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YALMRZBR.mjs";import"./chunk-6W3QTJS5.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-O2Z6XELS.mjs";import"./chunk-6W3QTJS5.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
